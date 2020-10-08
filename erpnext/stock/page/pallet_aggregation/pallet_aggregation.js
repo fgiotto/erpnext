98fbc8766492ed2c8734a4bd137ad109567a3a46
Francisco Giotto <GIOTTFR@vzc.global>
2020-10-08 17:17:09 -0400

    Paller Aggregation

--- erpnext/stock/page/pallet_aggregation/pallet_aggregation.js
+++ erpnext/stock/page/pallet_aggregation/pallet_aggregation.js
@@ -7,97 +7,212 @@ frappe.pages['pallet_aggregation'].on_page_load = function(wrapper) {
 
 	page.main.html(frappe.render_template("pallet_aggregation", {}));
 }
+var boxesPerCase = 0;
+var boxItem = "";
 
-//var boxesPerCase = 0;
-//var boxItem = "";
-
-//var deliveryNoteName = window.location.hash.substr(window.location.hash.lastIndexOf('/') + 1);
-//var selectedCaseSerial = "";
-//var selectedPalletSerial = "";
-//var obj = {
-//    CaseSerials: [],
-//    SelectedChildSerials: [],
-//    UnscannedChildSerials: []
-//};
-//var imported = document.createElement('script');
-//imported.src = 'https://d3js.org/d3.v5.min.js';
-//document.head.appendChild(imported);
+var deliveryNoteName = window.location.hash.substr(window.location.hash.lastIndexOf('/') + 1);
+var selectedCaseSerial = "";
+var selectedPalletSerial = "";
+var obj = {
+    CaseSerials: [],
+    SelectedChildSerials: [],
+    UnscannedChildSerials: []
+};
+var imported = document.createElement('script');
+imported.src = 'https://d3js.org/d3.v5.min.js';
+document.head.appendChild(imported);
 
 
-//$(document).ready(function () {
-//    let deliveryNote = await frappe.getDoc('Delivery Note', deliveryNoteName);
+$(document).ready(function () {
+    frappe.call({
+        method: "frappe.client.get",
+        args: {
+            doctype: "Delivery Note",
+            name: deliveryNoteName,
+        },
+        callback(r) {
+            if (r.message) {
+                var deliveryNote = r.message;
+                $('#DeliveryNoteLink').text(deliveryNote.name).attr('href', 'https://erp.lohxa.com/desk#Form/Delivery Note/' + deliveryNote.name);
+                $('#SalesOrderLink').text(deliveryNote.sales_order).attr('href', 'https://erp.lohxa.com/desk#Form/Sales Order/' + deliveryNote.sales_order);
+                $('#CustomerLink').text(deliveryNote.customer).attr('href', 'https://erp.lohxa.com/desk#Form/Customer/' + deliveryNote.customer);
+                $('#QuantityOrdered').text(deliveryNote.total_qty);
+                $('#SelectedBatch').text(deliveryNote.items[0].batch_no);
+                $('#TotalSerialNumbers').text(deliveryNote.items[0].actual_batch_qty);
+            }
 
-//    $('#DeliveryNoteLink').text(deliveryNote.name).attr('href', 'https://erp.lohxa.com/desk#Form/Delivery Note/' + deliveryNote.name);
-//    $('#SalesOrderLink').text(deliveryNote.sales_order).attr('href', 'https://erp.lohxa.com/desk#Form/Delivery Note/' + deliveryNote.sales_order);
-//    $('#CustomerLink').text(deliveryNote.customer).attr('href', 'https://erp.lohxa.com/desk#Form/Delivery Note/' + deliveryNote.customer);
+            RefreshPalletSerials();
+            RefreshTotalSerialsForDeliveryNote();
+        }
+    });
+});
 
-//    frappe.db.get_value('Sales Order', { name: deliveryNote.sales_order }, 'total_qty', (p) => {
-//        $('#QuantityOrdered').text(p.total_qty);
-//    });
+function AddNewPallet() {
+    if (window.confirm("Are you sure you want to create a new pallet?")) {
+        frappe.call({
+            "method": "erpnext.stock.page.pallet_aggregation.pallet_aggregation.add_pallet",
+            args: {
+                deliveryNoteName: deliveryNoteName
+            },
+            callback: function (r) {
+                RefreshPalletSerials();
+            }
+        });
+    }
+}
 
-//    frappe.call({
-//        "method": "erpnext.stock.page.delivery_scanning.delivery_scanning.get_pallet_serials",
-//        args: {
-//            deliveryNoteName: deliveryNoteName
-//        },
-//        callback: function (r) {
-//            $("#PalletTable tbody").empty();
-//            for (var i = 0; i < r.message.length; i++) {
-//                $("#PalletTable tbody").append(MakeCaseRowHtml(r.message[i]));
-//            }
+function RefreshPalletSerials() {
+    frappe.call({
+        "method": "erpnext.stock.page.pallet_aggregation.pallet_aggregation.get_pallet_serials",
+        args: {
+            deliveryNoteName: deliveryNoteName
+        },
+        callback: function (r) {
+            $("#PalletTable tbody").empty();
+            for (var i = 0; i < r.message.length; i++) {
+                $("#PalletTable tbody").append(MakePalletRowHtml(r.message[i]));
+            }
 
-//            $("#PalletTable tbody tr").click(function () {
-//                selectedPalletSerial = $(this).attr('data-case-serial');
-//                $("#PalletTable tbody tr").removeClass("selected");
+            $("#PalletTable tbody tr").click(function () {
+                selectedPalletSerial = $(this).attr('data-pallet-serial');
+                $("#PalletTable tbody tr").removeClass("selected");
 
-//                $(this).addClass("selected");
-//                $("#SelectedPallet").text(selectedPalletSerial);
+                $(this).addClass("selected");
+                $("#SelectedPallet").text(selectedPalletSerial);
 
-//                frappe.call({
-//                    "method": "erpnext.stock.page.delivery_scanning.delivery_scanning.get_pallet_child_serials",
-//                    args: {
-//                        serialNo: selectedPalletSerial
-//                    },
-//                    callback: function (r) {
-//                        var data = r.message;
-//                        SelectedChildSerials = [];
-//                        $("#ChildCaseTable tbody").empty();
-//                        if (data) {
-//                            for (var c = 0; c < data.length; c++) {
-//                                $("#ChildCaseTable tbody").append('<tr><td><span class="text-semibold">' + data[c].serial_no + '</span></td></tr>');
-//                            }
-//                        }
-//                        var casesScanned = $("#ChildCaseTable tbody tr").length;
+                RefreshPalletChildSerials();
+                RefreshTotalSerialsForDeliveryNote();
+            });
 
-//                        //                        RefreshUnscanned();
-//                        //SetDonutPercentage(boxesScanned / boxesPerCase);
+            $('.primary-action').append('<i class="visible-xs octicon octicon-plus"></i><span class="hidden-xs">Finalize</span>').removeClass('hide').click(function () {
+                frappe.call({
+                    "method": "erpnext.stock.page.pallet_aggregation.pallet_aggregation.set_pallet_aggregation_completed",
+                    args: {
+                        deliveryNoteName: deliveryNoteName
+                    },
+                    callback: function (r) {
+                        window.location = 'https://erp.lohxa.com/desk#List/Delivery%20Note/' + deliveryNoteName;
+                    }
+                });
+            });
+        }
+    });
+}
+function RefreshPalletChildSerials() {
+    frappe.call({
+        "method": "erpnext.stock.page.pallet_aggregation.pallet_aggregation.get_pallet_child_serials",
+        args: {
+            serialNo: selectedPalletSerial
+        },
+        callback: function (r) {
+            var serialCount = 0;
+            var data = r.message;
+            SelectedChildSerials = [];
+            $("#ChildCaseTable tbody").empty();
+            if (data) {
+                for (var c = 0; c < data.length; c++) {
+                    serialCount += parseInt(data[c][1]);
+                    $("#ChildCaseTable tbody").append('<tr><td><span class="text-semibold text-left">' + data[c][0] + '</span></td><td class="text-right"><span>' + data[c][1] + '</span></td></tr>');
+                }
+            }
+            var casesScanned = $("#ChildCaseTable tbody tr").length;
 
-//                        //                        $('#SelectedCaseScanned').text(boxesScanned);
-//                        //                        $('#BigBoxesScanned').text(boxesScanned);
+            $('.palletRow.selected .palletCasesScanned').text(casesScanned);
+            $('#TotalItemsScanned').text(serialCount);
+            $('#CasesScanned').text(casesScanned);
+        }
+    });
+}
+function RefreshTotalSerialsForDeliveryNote() {
+    frappe.call({
+        "method": "erpnext.stock.page.pallet_aggregation.pallet_aggregation.get_deliveryNote_serial_count",
+        args: {
+            deliveryNoteName: deliveryNoteName
+        },
+        callback: function (r) {
+            var data = r.message;
+            $('#TotalItemsScannedForDelivery').text(data);
+        }
+    });
+}
+function MakePalletRowHtml(arrayData) {
+    return '<tr class="palletRow" data-pallet-serial="' + arrayData[0] + '"><td><span class="text-semibold">' + arrayData[0] + '</span></td><td class="text-center"><span class="text-muted palletCasesScanned">' + arrayData[1] + '</span></td><td class="col-md-2 text-right"><button class="btn btn-primary btn-sm reprint"><i class="visible-xs octicon octicon-plus"></i><span class="hidden-xs">Print</span></button ></td></tr>';
+}
+function ScanCode(code) {
+    var serial = code.substring(18, 27);
+    ScanSerial(serial);
+}
+function ScanSerial(serial) {
+    if (serial.startsWith("3")) {
+        var trs = $("#PalletTable table tr");
+        for (var i = 0; i < trs.length; i++) {
+            if ($(trs[i]).attr("data-pallet-serial") == serial) {
+                $(trs[i]).click();
+            }
+        }
+    }
+    else if (serial.startsWith("1")) {
+        alert("You can't scan individual items on this screen");
+        return;
+    }
+    else {
+        frappe.call({
+            "method": "erpnext.stock.page.pallet_aggregation.pallet_aggregation.add_case_to_pallet",
+            args: {
+                palletSerial: selectedPalletSerial,
+                caseSerial: serial
+            },
+            callback: function (r) {
+                $("#PalletTable table tbody tr.selected").click();
+            }
+        });
+    }
+}
 
-//                        //                        $('#SelectedCaseLeft').text(boxesPerCase - boxesScanned);
-//                        //                        $('#BigBoxesLeft').text(boxesPerCase - boxesScanned);
+$.fn.codeScanner = function (options) {
+    var settings = $.extend({}, $.fn.codeScanner.defaults, options);
 
-//                        //                        $('#BigBoxesScanned').closest('div.panel').removeClass('bg-danger-400 bg-success-300 bg-slate-300').addClass((boxesScanned == boxesPerCase ? 'bg-success-300' : 'bg-danger-400'));
-//                        //                        $('#BigBoxesLeft').closest('div.panel').removeClass('bg-danger-400 bg-success-300 bg-slate-300').addClass((boxesScanned == boxesPerCase ? 'bg-success-300' : 'bg-danger-400'));
+    return this.each(function () {
+        var pressed = false;
+        var chars = [];
+        var $input = $(this);
 
-//                        $('.caseRow.selected .caseBoxesScanned').text(casesScanned);
-//                    }
-//                });
-//            });
+        $(window).keypress(function (e) {
+            var keycode = (e.which) ? e.which : e.keyCode;
+            if ((keycode >= 65 && keycode <= 90) ||
+                (keycode >= 97 && keycode <= 122) ||
+                (keycode >= 48 && keycode <= 57)
+            ) {
+                chars.push(String.fromCharCode(e.which));
+            }
+            // console.log(e.which + ":" + chars.join("|"));
+            if (pressed == false) {
+                setTimeout(function () {
+                    if (chars.length >= settings.minEntryChars) {
+                        var barcode = chars.join("");
+                        settings.onScan($input, barcode);
+                    }
+                    chars = [];
+                    pressed = false;
+                }, settings.maxEntryTime);
+            }
+            pressed = true;
+        });
 
-//            $('.primary-action').append('<i class="visible-xs octicon octicon-plus"></i><span class="hidden-xs">Finalize</span>').addClass('disabled').removeClass('hide').click(function () {
-//                frappe.call({
-//                    "method": "erpnext.stock.page.delivery_scanning.delivery_scanning.set_pallet_aggregation_completed",
-//                    args: {
-//                        deliveryNoteName: deliveryNoteName
-//                    },
-//                    callback: function (r) {
-//                        window.location = 'https://erp.lohxa.com/desk#List/Delivery%20Note/' + deliveryNoteName;
-//                    }
-//                });
-//            });
-//            //RefreshUnscanned();
-//        }
-//    });
-//});
+        $(this).keypress(function (e) {
+            if (e.which === 13) {
+                e.preventDefault();
+            }
+        });
+
+        return $(this);
+    });
+};
+
+$.fn.codeScanner.defaults = {
+    minEntryChars: 8,
+    maxEntryTime: 100,
+    onScan: function ($element, barcode) {
+        $element.val(barcode);
+    }
+};
--- erpnext/stock/page/pallet_aggregation/pallet_aggregation.py
+++ erpnext/stock/page/pallet_aggregation/pallet_aggregation.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.utils import cint
+from frappe.model.naming import make_autoname
 
 @frappe.whitelist()
 def reprint_unscanned(stockEntryName):
@@ -66,20 +67,113 @@ def get_pallet_serials(deliveryNoteName):
 		from `tabSerial No` as serialNo
 		left join `tabChild Serial No Details` as childSerial
 		on childSerial.parent = serialNo.name
-		where serialNo.delivery_note=%(deliveryNote)s
+		where serialNo.delivery_document_no=%(deliveryNote)s
 		and serialNo.item_code=%(item)s
 		group by serialNo.name
 		order by serialNo.creation desc""",
 		{
 			"item": "PALLET",
-			"name": deliveryNoteName
+			"deliveryNote": deliveryNoteName
 		})
 
 	return result
 
+import json
+
+@frappe.whitelist()
+def get_child_serial_count(serialNos):
+	serials = serialNos.split(',')
+	result = frappe.db.sql("""select count(*)
+		from `tabChild Serial No Details`
+		where parent in (%s)"""  % (','.join(['%s'] * len(serials))), tuple(serials))
+	return result
+
+@frappe.whitelist()
+def get_pallet_serial_count(serialNos):
+	serials = serialNos.split(',')
+
+	palletResult = frappe.db.sql("""select serial_no
+		from `tabChild Serial No Details`
+		where parent in (%s)"""  % (','.join(['%s'] * len(serials))), tuple(serials))
+
+	result = frappe.db.sql("""select count(*)
+		from `tabChild Serial No Details`
+		where parent in (%s)"""  % (','.join(['%s'] * len(palletResult))), tuple(palletResult))
+
+	return result
+
+@frappe.whitelist()
+def get_deliveryNote_serial_count(deliveryNoteName):
+	deliveryNoteResult = frappe.db.sql("""select serial_no
+		from `tabSerial No`
+		where delivery_document_no=%(deliveryNote)s
+		and item_code=%(item)s""",
+		{
+			"item": "PALLET",
+			"deliveryNote": deliveryNoteName
+		})
+
+	palletResult = frappe.db.sql("""select serial_no
+		from `tabChild Serial No Details`
+		where parent in (%s)"""  % (','.join(['%s'] * len(deliveryNoteResult))), tuple(deliveryNoteResult))
+
+	result = frappe.db.sql("""select count(*)
+		from `tabChild Serial No Details`
+		where parent in (%s)"""  % (','.join(['%s'] * len(palletResult))), tuple(palletResult))
+
+	return result
+
+@frappe.whitelist()
+def get_deliveryNote_serial_numbers(deliveryNoteName):
+	deliveryNoteResult = frappe.db.sql("""select serial_no
+		from `tabSerial No`
+		where delivery_document_no=%(deliveryNote)s
+		and item_code=%(item)s""",
+		{
+			"item": "PALLET",
+			"deliveryNote": deliveryNoteName
+		})
+
+	palletResult = frappe.db.sql("""select serial_no
+		from `tabChild Serial No Details`
+		where parent in (%s)"""  % (','.join(['%s'] * len(deliveryNoteResult))), tuple(deliveryNoteResult))
+
+	result = frappe.db.sql("""select serial_no
+		from `tabChild Serial No Details`
+		where parent in (%s)"""  % (','.join(['%s'] * len(palletResult))), tuple(palletResult))
+
+	return result
+
+@frappe.whitelist()
+def add_pallet(deliveryNoteName):
+	item_det = get_item_details('PALLET')
+
+	sr = frappe.new_doc("Serial No")
+	sr.warehouse = None
+	sr.flags.ignore_permissions = True
+	sr.serial_no = make_autoname(item_det.serial_no_series, 1)
+	sr.item_code = 'PALLET'
+	sr.company = 'Lohxa LLC'
+	sr.delivery_document_type = 'Delivery Note'
+	sr.delivery_document_no = deliveryNoteName
+
+	sr.insert()
+
+	return sr.name
+
+def get_item_details(item_code):
+	return frappe.db.sql("""select name, has_batch_no, docstatus,
+		is_stock_item, has_serial_no, serial_no_series
+		from tabItem where name=%s""", item_code, as_dict=True)[0]
+
 @frappe.whitelist()
 def set_pallet_aggregation_completed(deliveryNoteName):
-	frappe.db.set_value("Delivery Note",deliveryNoteName,"pallet_aggregation_completed",1)
+	serialNumbers = get_deliveryNote_serial_numbers(deliveryNoteName)
+	deliveryNote = frappe.get_doc("Delivery Note", deliveryNoteName)
+	deliveryNote.items[0].qty = len(serialNumbers)
+	deliveryNote.items[0].serial_no = '\n'.join(''.join(elems) for elems in serialNumbers)
+	deliveryNote.save(ignore_permissions=True)
+	#frappe.db.set_value("Delivery Note",deliveryNoteName,"pallet_aggregation_completed",1)
 
 @frappe.whitelist()
 def make_reprint_request(serial_no, item):
@@ -91,30 +185,33 @@ def make_reprint_request(serial_no, item):
 
 @frappe.whitelist()
 def get_pallet_child_serials(serialNo):
-	result = frappe.db.sql("""select serial_no
-		from `tabChild Serial No Details`
-		where parent=%(name)s
-		order by serial_no asc""",
+	result = frappe.db.sql("""select parent.serial_no, count(child.name) as count
+		from `tabChild Serial No Details` as parent 
+		left join `tabChild Serial No Details` as child 
+		on child.parent = parent.serial_no 
+		where parent.parent=%(name)s 
+		group by parent.serial_no 
+		order by parent.serial_no asc""",
 		{
 			"name": serialNo
-		}, as_dict=True)
+		})
 
 	return result
 
 @frappe.whitelist()
-def add_serial_to_case(caseSerial, child_serial):
+def add_case_to_pallet(palletSerial, caseSerial):
 	result = frappe.db.sql("""select count(name)
 		from `tabChild Serial No Details`
 		where serial_no=%(name)s""",
 		{
-			"name": child_serial
+			"name": caseSerial
 		})[0][0]
 	if(int(result) > 0):
-		frappe.throw("Box was already added to another Case. Try again.")
+		frappe.throw("Case was already added to another Pallet. Try again.")
 
-	case = frappe.get_doc("Serial No", caseSerial)
+	case = frappe.get_doc("Serial No", palletSerial)
 	case.append("sub_item_serial_numbers", {
-		'serial_no': child_serial
+		'serial_no': caseSerial
 	})
 	case.save(ignore_permissions=True)
 	return 'Success'
